953cabb4b6fba20e372a856bdd1d718bf626a3d7
Daniel.Reis <73089512+Danny-codes@users.noreply.github.com>
2022-01-05 14:46:38 -0300

    dealing with sockets

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,41 @@
+const chatForm = document.getElementById('chat-form');
+const messagesDiv = document.querySelector('.chat-message');
+
 const socket = io()
+
+//Message from server
+socket.on('message', message => {
+    console.log(message)
+    outputMessage(message);
+
+    //scroll down
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+
+    //clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+});
+
+//Message submit
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //Get message Text
+    const msg = e.target.elements.msg.value;
+
+    //Emit message to server
+    socket.emit('chatMessage', msg);
+})
+
+//Output message to DOM
+outputMessage = (message) => {
+    const div = document.createElement('div')
+    div.classList.add('message');
+    div.innerHTML = `
+    <p class="message-info">Mary <span>9:15pm</span></p>
+    <p>
+        ${message}
+    </p>
+    `
+    document.querySelector('.chat-message').appendChild(div)
+}
