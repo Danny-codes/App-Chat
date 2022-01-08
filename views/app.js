c1575f4f9851a85651b090794dcff97b3157e694
Daniel.Reis <73089512+Danny-codes@users.noreply.github.com>
2022-01-08 18:13:30 -0300

    redirecting user after registration completed

--- views/app.js
+++ views/app.js
@@ -1,18 +1,24 @@
 
 const url = 'http://localhost:3000/api/user'
 
- login = async(req,res) => {
-   // const body = {
-   //     email: document.getElementById('emailField').value,
-   //     password: document.getElementById('passwordField').value
-    //}
+ register = async(req,res) => {
+    const body = {
+        email: document.getElementById('emailField').value,
+        password: document.getElementById('passwordField').value,
+        name: document.getElementById('nameField').value,
+        bio: document.getElementById('bioField').value
+    }
     try{
-       const response = await axios.get(`${url}`)
-       console.log(response)
+       const response = await axios.post(`${url}`, body)
+       if(response.status == 200){
+          window.location.replace('./home.html')
+       }
+       else{
+           console.log('aqui')
+       }
     }catch(Error){
-        console.log(response)
+        console.log('Ocorreu um erro')
     }
   
 }
 
-login()
