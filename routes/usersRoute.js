ddeaf30d0a6c139d40df075c732412d7b5877759
Daniel.Reis <73089512+Danny-codes@users.noreply.github.com>
2022-01-11 16:56:06 -0300

    adding authMiddleware to routes

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router =  express.Router()
+const authMiddleware = require('../middlewares/authMiddleware')
+
 
 const 
     UserServices
  = require('../controllers/usersController')
 
-router.route('/').get(UserServices.getAllUsers).post(UserServices.createUser)
-router.route('/:id').get(UserServices.getAUser).patch(UserServices.updateUser).delete(UserServices.deleteUser)
+router.route('/').get(authMiddleware,UserServices.getAllUsers).post(UserServices.createUser)
+router.route('/:id').get(authMiddleware,UserServices.getAUser).patch(authMiddleware,UserServices.updateUser).delete(authMiddleware,UserServices.deleteUser)
 
 
 module.exports = router
