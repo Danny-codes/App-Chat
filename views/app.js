73d9d6fc9c3cb98168bc2bc8b23277fbdc4dbf36
Daniel.Reis <73089512+Danny-codes@users.noreply.github.com>
2022-01-12 15:50:02 -0300

    getting token and saving it in localStorage

--- views/app.js
+++ views/app.js
@@ -1,21 +1,49 @@
 
 const url = 'http://localhost:3000/api/user'
 
- register = async(req,res) => {
+let axiosConfig = {
+    headers: {
+        Authotization: "Bearer " + localStorage.getItem.token
+    }
+}
+ register = async() => {
     const body = {
         email: document.getElementById('emailField').value,
         password: document.getElementById('passwordField').value,
         name: document.getElementById('nameField').value,
         bio: document.getElementById('bioField').value
     }
+    
+       axios.post(`${url}`, body)
+        .then(req => {
+            const token = req.data.token
+            const name = req.data.name
+            localStorage.setItem('token', token);
+            localStorage.setItem('User_Name', name);
+            axiosConfig.headers.Authotization = "Bearer " + localStorage.getItem("token");
+        })
+        .catch(err => {
+            res.json(err)
+        })
+      
+  
+}
+
+login = async() => {
+    const body = {
+        email: document.getElementById('emailField').value,
+        password: document.getElementById('passwordField').value,
+     
+    }
     try{
-       const response = await axios.post(`${url}`, body)
-       if(response.status == 200){
-          window.location.replace('./home.html')
-       }
-       else{
-           console.log('aqui')
-       }
+       axios.post(`${url}/auth`, body).then(req => {
+        const token = req.data.token
+        const name = req.data.name
+        localStorage.setItem('token', token);
+        localStorage.setItem('User_Name', name);
+        axiosConfig.headers.Authotization = "Bearer " + localStorage.getItem("token");
+       })
+      
     }catch(Error){
         console.log('Ocorreu um erro')
     }
