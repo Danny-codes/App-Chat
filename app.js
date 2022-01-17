5e52d34399cb92f35e091dd1ff6663278c83f2b7
Daniel.Reis <73089512+Danny-codes@users.noreply.github.com>
2022-01-17 11:52:19 -0300

    adding room route

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const cors = require('cors');
 require("dotenv").config()
 const app = express()
 
-var http = require('http').createServer(app),
-    sockets = require('./sockets')
-
 app.use(cors())
 
-require('./sockets')
+var http = require('http').createServer(app),
+sockets = require('./sockets')
+
+//static
+app.use(express.static('./public'));
+
+
+//view engine
+app.set('view engine', "ejs")
 
 //body parser
 app.use(express.urlencoded({ extends: false }));
@@ -18,21 +23,15 @@ app.use(express.json());
 const {DB} = require('./database/db.js')
 const users = require('./routes/usersRoute')
 const auth = require('./routes/authRoute')
+const room = require('./routes/roomRoute')
 const adress = require('./routes/adressRoute')
-
-
 
 //routes
 app.use('/api/user', users)
 app.use('/api/user', auth)
+app.use('/home', room)
 app.use('/', adress)
 
-//static
-
-app.use(express.static('./public'));
-
-//view engine
-app.set('view engine', "ejs")
 
 
 const port = process.env.PORT
